9ff7958a07f6e0aa9e95a51a4d50204a9f0dab3f
Jan Kotvaš <jan.kotvas96@gmail.com>
2026-04-10 20:11:47 +0200

    added the color pallete and started cleaning up the files

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,3 @@
-import { ThemeSwitcher } from "@/components/theme-switcher";
-
 export default function Home() {
-  return (
-    <main className="min-h-screen flex flex-col">
-      <div className="flex-1 w-full flex flex-col gap-20 p-10">
-        <ThemeSwitcher />
-      </div>
-    </main>
-  );
+  return <main></main>;
 }
